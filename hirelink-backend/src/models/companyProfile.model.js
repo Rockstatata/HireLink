50bdb0a82466e3a5251fb9fafcf0dc71510b54ee
mayer_doa_coder <mdmatin401@gmail.com>
2025-10-11 16:19:36 +0600

    fix: update aiUseLimit default value

--- hirelink-backend/src/models/companyProfile.model.js
+++ hirelink-backend/src/models/companyProfile.model.js
@@ -25,7 +25,7 @@ const companyProfileSchema = new Schema({
   jobListings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job" }],
   companySocialProfiles: socialProfilesSchema,
   employeeBenefits: [String],
-  aiUseLimit: { type: Number, default: 1 },
+  aiUseLimit: { type: Number, default: 100 },
 });
 
 export const CompanyProfile = mongoose.model(
